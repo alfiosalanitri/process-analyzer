62ba19d8beaeb867ba23485ec4d5fedbea16b7b3
alfiosalanitri <hello@alfiosalanitri.it>
2022-09-17 16:36:35 +0200

    first commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# process-analyzer
+Analyze the ram and cpu usage of a process in real time.
+
+![preview](./preview.png)
+
+## Installation
+- `sudo ln -s /path/to/process-analyzer.sh /usr/local/bin/process-analyzer`
+
+## Usage
+- `process-analyzer [-n] 5` to refresh every 5 seconds
+
+# Author:
+process-analyzer is written by Alfio Salanitri www.alfiosalanitri.it and are licensed under the terms of the GNU General Public License, version 2 or higher.
+
---
+++ process-analyzer.sh
@@ -0,0 +1,66 @@
+#!/bin/bash
+# Analyze the ram and cpu usage of a process in real time.
+# written by Alfio Salanitri <www.alfiosalanitri.it> and are licensed under MIT license. 
+
+display_help() {
+cat << EOF
+Copyright (C) 2022 by Alfio Salanitri
+Website: https://github.com/alfiosalanitri/process-analyzer
+Usage: $(basename $0) -n 5
+Options
+
+-n, --interval seconds  
+    Specify update interval. The command will not allow quicker than 1 second interval 
+
+-h, --help  
+    show this help
+-------------
+EOF
+exit 0
+}
+
+# display memory and ram used
+analyze_me() {
+ ps -p $1 -o cmd,size,%cpu | tail -n 1 | awk '{name=$1;ram=$2/1024;cpu=$3;printf "Name: [%s] - RAM: [%5.2f Mb] - CPU: [%5.2f%]\n", name, ram, cpu}'
+}
+
+# inteval seconds
+interval=.1
+
+# check user options
+while [ $# -gt 0 ] ; do
+  case $1 in
+    -h | --help) display_help ;;
+    -n | --interval)
+      if ! [[ $2 =~ ^[0-9]+$ ]] ; then
+        echo "Interval must be an integear."
+        exit 1
+      fi
+      if [ $2 -gt 1 ]; then
+        interval=$2
+      fi
+    ;;
+  esac
+  shift
+done
+
+# get process name
+read -p "Type the process name: "
+pid=$(pidof -s "$REPLY")
+if [ "" == "$pid" ]; then
+  echo "Sorry, but this process doesn't exists. Try again."
+  exit 1
+fi
+
+# start
+echo "-----------------"
+echo "Process Analyzer"
+echo "-----------------"
+echo "press CTRL+C to exit"
+echo ""
+while true; do
+  usage=$(analyze_me $pid)
+  echo -n -e "$(date) | $usage\r"
+  sleep $interval
+done
+exit 0
